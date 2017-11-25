7d8f0b412edff50d58bb34c6e76a1a809c1ef9ef
STONE <sjhstone@live.com>
2017-11-26 06:17:37 +0800

    antd ui hello world

---
+++ bmw_ui/src/layout/mainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { DatePicker } from 'antd';
+
+export default class BmwMainLayout extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        <DatePicker />
+    }
+}
